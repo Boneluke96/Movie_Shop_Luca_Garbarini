80c53b2dbe7f72a29e9b64dd9a6c8a917e283525
Luca Garbarini <Lucagarbarini@outlook.it>
2023-10-13 16:46:10 +0200

    movie shop

---
+++ annunci.js
@@ -0,0 +1,127 @@
+let genresWrapper = document.querySelector('#genresWrapper');
+let cardsWrapper = document.querySelector('#cardsWrapper');
+let priceInput = document.querySelector('#priceInput');
+let priceNumbers = document.querySelector('#priceNumbers');
+let wordInput = document.querySelector('#wordInput');
+
+
+fetch('./annunci.json').then((response) => response.json()).then((data) => {
+    data.forEach((element) => {
+        element.price = element.price.split('')
+        element.price.shift()
+        element.price = element.price.join('').replace(',','.')
+    })
+    
+    function setGenres(){
+        let genres = data.map((announce) => announce.genres);
+        console.log(genres);
+
+        let uniqueGenres = [];
+
+        genres.forEach((genre) => {
+            if(!uniqueGenres.includes(genre)){
+                uniqueGenres.push(genre);
+            }
+        });
+
+        uniqueGenres.forEach((genre) => {
+            let div = document.createElement('div');
+            div.classList.add('form-check');
+            div.innerHTML = 
+            `<input class="form-check-input" type="radio" name="genres" id="${genre}">
+            <label class="form-check-label" for="${genre}">
+              ${genre}
+            </label>`
+
+            genresWrapper.appendChild(div);
+        });
+    }
+    setGenres();
+
+
+    function showCards(array){
+
+        array.sort((a, b) => b.price - a.price);
+
+        cardsWrapper.innerHTML = '';
+
+        array.forEach((announce) => {
+            let div = document.createElement('div');
+            div.classList.add("card-custom");
+            div.innerHTML = `
+            <h2>${announce.name}</h2>
+            <h3 class="card-title">${announce.genres}</h3>
+            <p class="card-tx">€ ${announce.price}</p>
+            `
+            cardsWrapper.appendChild(div);
+        });
+    }
+    showCards(data);
+
+    let radios = document.querySelectorAll('.form-check-input');
+
+    function filterByCategory(array){
+        let checked = Array.from(radios).find((button) => button.checked);
+        let genres = checked.id;
+
+        if(genres != 'all'){
+            let filtered = array.filter((announce) => announce.genres == genres);
+            if(check){
+                setPriceInput(filtered);
+                check = false;
+            }
+            return filtered
+        }else{
+            if(check){
+                setPriceInput(data);
+                check = false;
+            }
+            return array
+        }
+    }
+
+    let check = false;
+    
+    radios.forEach((button) => {
+        button.addEventListener('click', () => {
+            check = true
+            globalFilter();
+        })
+    })
+
+    function setPriceInput(array){
+        let maxPrice = array[0].price;
+        priceInput.max = Math.ceil(maxPrice);
+        priceInput.value = Math.ceil(maxPrice);
+        priceNumbers.innerHTML = maxPrice;
+    }
+    setPriceInput(data);
+
+    priceInput.addEventListener('input', () => {
+        priceNumbers.innerHTML = priceInput.value;
+        globalFilter();
+    })
+
+    function filterByPrice(array){
+        let filtered = array.filter((announce) => +announce.price <= +priceInput.value)
+        return filtered;
+    }
+
+    wordInput.addEventListener('input', () => {
+        globalFilter();
+    })
+
+    function filterByWord(array){
+        let filtered = array.filter((announce) => announce.name.toLowerCase().includes(wordInput.value.toLowerCase()))
+        return filtered;
+    }
+
+    function globalFilter(){
+        let filterCategory = filterByCategory(data);
+        let filterPrice = filterByPrice(filterCategory);
+        let filterWord = filterByWord(filterPrice);
+
+        showCards(filterWord);
+    }
+
+});
---
+++ script.js
@@ -0,0 +1,133 @@
+//catture elementi
+let navbar = document.querySelector("#navbar");
+let links = document.querySelectorAll('.nav-link');
+let logo = document.querySelector("#logo");
+
+window.addEventListener('scroll', () => {
+    let scrolled = window.scrollY;
+    
+    if(scrolled > 0){
+        changeNavbar("navbar-blur", "movie-red", "var(--colorThird)");
+    }else{
+        navbar.classList.remove('navbar-blur');
+        changeNavbar("navbar-custom", "movie-white", "var(--colorPrimary)",);
+    }
+});
+
+function changeNavbar(background, imgLogo, color1, color3){
+    navbar.classList.add(background);
+    
+    logo.src = `http://127.0.0.1:5500/media/${imgLogo}.png`;
+    
+    links.forEach((link) => {
+        link.style.color = color1;
+    });
+    
+    logo.src = `http://127.0.0.1:5500/media/${imgLogo}.png`;
+    
+    links.forEach((link) => {
+        link.style.color = color3;
+    });
+}
+
+
+
+//chiamata asincrona numeri incrementali
+let primoNumero = document.querySelector("#primoNumero");
+let secondoNumero = document.querySelector('#secondoNumero');
+let terzoNumero = document.querySelector('#terzoNumero');
+
+let confirm = false;
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting && confirm == false){
+            createInterval(1000, primoNumero, 3);
+            createInterval(1500, secondoNumero, 1);
+            createInterval(100, terzoNumero, 50);
+            confirm = true;
+        }
+    })
+});
+observer.observe(primoNumero);
+
+function createInterval(number, element, timing){
+    let counter = 0
+    let interval = setInterval(() => {
+        if(counter < number){
+            counter++
+            element.innerHTML = counter;
+        }else{
+            clearInterval(interval)
+        }
+    }, timing);
+}
+
+//recensioni swiper
+let reviews = [
+    {name : "Angelo", description : "Ottimi prodotti"},
+    {name : "Luca", description : "tutto in ottime condizioni non credevo!"},
+    {name : "Riccardo", description : "5 stelle meritate per loro"},
+    {name : "Antonio", description : "servizio ottimo danno anche qualche adesivo!"},
+    {name : "Marco", description : "Ho visto di meglio ma gli darò una chance"},
+    {name : "Fulvio", description : "Sito interessante..."}
+]
+
+let swiperWrapper = document.querySelector('#swiperWrapper');
+let userName = document.querySelector('#userName');
+let userDescription = document.querySelector('#userDescription');
+let addReviewBtn = document.querySelector('#addReviewBtn');
+
+addReviewBtn.addEventListener('click', () => {
+    reviews.push({name : userName.value, description : userDescription.value});
+    generateCard();
+    userName.value = '';
+    userDescription.value = '';
+    swiper.update();
+})
+
+function generateCard(){
+    swiperWrapper.innerHTML = '';
+    reviews.forEach((review) => {
+        let div = document.createElement('div');
+        div.classList.add("swiper-slide");
+        div.innerHTML = 
+        `<div class="review-card">
+        <p class="h3">${review.name}</p>
+        <p class="lead">${review.description}</p>
+        </div>`
+        swiperWrapper.appendChild(div);
+    });
+}
+generateCard();
+
+
+
+//swiper JS
+const swiper = new Swiper('.swiper', {
+
+    // Optional parameters
+    loop: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
